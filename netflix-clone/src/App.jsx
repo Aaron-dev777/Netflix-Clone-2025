97dec0053666a4da06ebd5bcf26e08b7b76b1519
Aaron-dev777 <aaron.tsegai@gmail.com>
2025-02-18 18:31:44 +0400

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
+import Home from "./page/Home/Home";
+
 
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>HELLO</h1>
-      </div>
+      <Home/>
     </>
   );
 }
