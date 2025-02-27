f897e155ac97df598653bae24e267e330559d926
Aaron-dev777 <aaron.tsegai@gmail.com>
2025-02-27 21:36:51 +0400

    first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HEADER</h1>
+      <h1>FOOTER</h1>
+    </div>
+  )
+}
+
+export default App
